b2b3c2e8636daf2f3122629e4235ba1bb133e4a8
devsarswat <vasudevsarswat6@gmail.com>
2023-06-22 18:50:29 +0530

    card update

--- src/Component/GetData.jsx
+++ src/Component/GetData.jsx
@@ -1,21 +1,42 @@
 import React, { useState } from 'react';
-import { Card, CardContent, CardMedia, Typography, Button } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Button, TextField } from '@mui/material';
 import data from '../Data/CoffeeData.json';
+
 
 const ITEMS_PER_PAGE = 6;
 
 const GetData = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1); // Reset the current page when the search query changes
+  };
+
+  const filteredVarieties = data.varieties.filter((variety) =>
+    variety.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const displayedVarieties = data.varieties.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+  const displayedVarieties = filteredVarieties.slice(startIndex, startIndex + ITEMS_PER_PAGE);
 
   return (
-    <div>
+    <div className='Sbar'>
+      <div className="search-bar-container">
+        <TextField
+          label="Search"
+          variant="outlined"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="search-bar"
+        />
+      </div>
+
       <div className="card-container">
         {displayedVarieties.map((variety, index) => (
           <Card key={index} className="card">
@@ -54,7 +75,7 @@ const GetData = () => {
 
       <div className="pagination-container">
         {Array.from(
-          { length: Math.ceil(data.varieties.length / ITEMS_PER_PAGE) },
+          { length: Math.ceil(filteredVarieties.length / ITEMS_PER_PAGE) },
           (_, index) => (
             <Button
               key={index}
